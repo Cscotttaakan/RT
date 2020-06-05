65453cef6eab73a8b445a29ff1c74570eb04bac8
Craig Scott <cscott_taakan@yahoo.com>
2020-06-05 00:14:12 -0400

    Init RT

---
+++ RT/RT.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Craig Scott on 6/4/20.
+//
+
+#include <Eigen/Eigen>
+#include <stb_image_write.h>
+
+int main(int argc, char *argv[])
+{
+    Eigen::Vector2f vec2{2.0f,1.0f};
+    
+    return 0;
+}
